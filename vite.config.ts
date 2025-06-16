2b6bc412cce44753ebc1d02315c2a038ad7d473d
Sanshiro Hikawa <106821913+sanpicule@users.noreply.github.com>
2025-06-17 01:05:24 +0900

    Fix Vite build configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,6 +11,7 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
+      input: path.resolve(__dirname, 'src/main.tsx'),
       output: {
         manualChunks: {
           vendor: ['react', 'react-dom'],
